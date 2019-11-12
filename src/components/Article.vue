0421e90ba091e34e8e0cac1ebcea6c674fe3be5d
shotasan <wondermoai@gmail.com>
2019-11-12 10:49:15 +0900

    first commit

---
+++ src/components/Article.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>記事コード：{{ aid }}</div>
+    <span>
+      <router-link v-bind:to="'/article/' + aid + '/pages/1'">Page: 1</router-link>
+    </span>|
+    <span>
+      <router-link v-bind:to="'/article/' + aid + '/pages/2'">Page: 2</router-link>
+    </span>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "article",
+  props: {
+    aid: String
+  }
+};
+</script>
