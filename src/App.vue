7f6a73cad88941e92b0e0d07f080b9c9d2b57d0a
shotasan <wondermoai@gmail.com>
2019-11-12 13:58:16 +0900

    ナビゲーションガードを実装

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
-      <router-link to="/article/108">Article</router-link>
+      <router-link to="/article/108">Article</router-link>|
+      <router-link to="/article/13">Article</router-link>
     </div>
     <router-view />
   </div>
--- src/components/Article.vue
+++ src/components/Article.vue
@@ -12,8 +12,30 @@
 </template>
 
 <script>
+let timeGuard = function(to, from, next) {
+  let data = {
+    13: new Date(2019, 10, 30),
+    108: new Date(2018, 10, 30)
+  };
+
+  let limit = data[to.params.aid]
+    ? data[to.params.aid]
+    : new Date(2999, 12, 31);
+
+  let curret = new Date();
+
+  if (limit && limit.getTime() > curret.getTime()) {
+    next();
+  } else {
+    window.alert("記事の公開期限が過ぎています。");
+    next(false);
+  }
+};
+
 export default {
   name: "article",
+  beforeRouteEnter: timeGuard,
+  beforeRouteUpdate: timeGuard,
   props: {
     aid: String
   }
